18f5f662641325d5a653fa6884670fc64e22f632
student <student@safebear.co.uk>
2019-10-03 09:42:52 +0100

    sorted some more art

--- e2e/support/hooks.ts
+++ e2e/support/hooks.ts
@@ -1,20 +1,18 @@
 import { Before, After, Status } from "cucumber";
 import { browser } from "protractor";
 
-Before({timeout: 100 * 1000}, async function (){
-    //Get command means navigate to browser
+Before({timeout: 100 * 1000}, async function() {
+    // Get command means navigate to browser
     await browser.get("");
-})
+});
 
-After({timeout: 100 * 1000}, async function(scenario){
+After({timeout: 100 * 1000}, async function(scenario) {
 
-    //Here we take a screenshot if the scenario fails
-    //and if it's attached to the test report
+    // Here we take a screenshot if the scenario fails
+    // and if it's attached to the test report
 
-    if(scenario.result.status === Status.FAILED) {
-        const screenshot = await browser.takescreenshot();
-        this.attach(screenshot, "image/png");
+    if (scenario.result.status === Status.FAILED) {
+        const screenShot = await browser.takeScreenshot();
+        this.attach(screenShot, "image/png");
     }
-
-})
-
+});
